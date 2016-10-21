f9b3e1bdd3798adc04afbb1c0d245916cd6208f8
modern12 f <modern12@o2.pl>
2016-10-21 15:01:09 +0200

    firma
    firma 21.10.16

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,8 +6,13 @@ public class GameManager : MonoBehaviour {
     public static GameManager instance;
 
     public GameObject groundTilePrefab;
-    public int mapWidth = 32;
-    public int mapHeight = 32;
+    public GameObject asphaltTilePrefab;
+    public GameObject concreteSlabTilePrefab;
+
+    [Range(64, 256)]
+    public int mapWidth = 64;
+    [Range(64, 256)]
+    public int mapHeight = 64;
 
     void Awake()
     {
@@ -28,8 +33,23 @@ public class GameManager : MonoBehaviour {
         {
             for (int j = 0; j < mapWidth; j++)
             {
-                GameObject go = (GameObject)Instantiate(groundTilePrefab, new Vector3 (i,j,0), Quaternion.identity);
-                go.transform.parent = GameObject.Find("GroundTiles").transform;
+
+                if (i == 3 || i == 9)
+                {
+                    GameObject go = (GameObject)Instantiate(concreteSlabTilePrefab, new Vector3(i, j, 0), Quaternion.identity);
+                    go.transform.parent = GameObject.Find("GroundTiles").transform;
+                }
+                else if (i >= 4 && i <= 8)
+                {
+                    GameObject go = (GameObject)Instantiate(asphaltTilePrefab, new Vector3(i, j, 0), Quaternion.identity);
+                    go.transform.parent = GameObject.Find("GroundTiles").transform;
+                }
+                else
+                {
+                    GameObject go = (GameObject)Instantiate(groundTilePrefab, new Vector3 (i,j,0), Quaternion.identity);
+                    go.transform.parent = GameObject.Find("GroundTiles").transform;
+                }
+                
             }
         }
     }
